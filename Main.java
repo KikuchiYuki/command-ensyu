f2283fb429a0dfcfb59a9edcf3fe1c8a0ad4bd6f
yuki <yuuki.kikuchi.sys23@morijyobi.ac.jp>
2023-11-09 14:44:47 +0900

    2人分のBMI処理に変更

--- Main.java
+++ Main.java
@@ -2,15 +2,23 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("身長(m)と体重を入力↓");
+        System.out.println("1人目の身長(m)と体重を入力↓");
         Double height1 = Double.parseDouble(sc.nextLine());
         Double weight1 = Double.parseDouble(sc.nextLine());
+        System.out.println("2人目の身長と体重を入力↓");
+        Double height2 = Double.parseDouble(sc.nextLine());
+        Double weight2 = Double.parseDouble(sc.nextLine());
 
         Double bmi1 = weight1 / (height1 * height1) ;
+        Double bmi2 = weight2 / (height2 * height2) ;
         String sv1 = String.format("%.2f",bmi1);
+        String sv2 = String.format("%.2f",bmi2);
 
-        System.out.println("BMIは" + sv1 + "です。");
+        System.out.println("1人目のBMIは" + sv1 + "です。");
         bmi(bmi1);
+        System.out.println("2人目のBMIは" + sv2 + "です。");
+        bmi(bmi2);
+
 
     }
 
